bdec7d176929e1d8a3473febf416fa35b328211a
Paulose <prince.paulose@capgemini.com>
2023-04-06 12:06:39 +0530

    data base connection

--- index.ts
+++ index.ts
@@ -7,18 +7,17 @@ import { mainRoutes } from "./src/routes";
 const app = express();
 app.use(json());
 app.use(mainRoutes);
-
-mongoose.connect(
-  "mongodb://localhost:27017/shopping-portal",
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  () => {
-    console.log("connected to database");
-  }
-);
+console.log("process",process.env)
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.dbUsername}:${process.env.dbPassword}@cluster0.w1wigmx.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Connected to database :)");
+  })
+  .catch((reason) => {
+    console.log("Failed to connect to database :(", reason);
+  });
 
 app.listen(4000, () => {
   console.log("server is listening on port 4000");
